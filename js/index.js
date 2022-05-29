c1ee45e29d4e446e0d4186214a76a8a903f74b6f
Tzu Yu Chiu <tzuyujulia@gmail.com>
2022-05-29 18:30:46 +0200

    fixed palyer image

--- js/index.js
+++ js/index.js
@@ -82,15 +82,15 @@ class Player {
     this.position = position;
     this.speed = speed;
     this.monoImage = new Image();
-    this.monoImage.src = './images/cat.svg';
+    this.monoImage.src = './images/cat.png';
   }
   draw() {
     ctx.drawImage(
       this.monoImage,
       this.position.x * 70,
       this.position.y * 70,
-      this.monoImage.width / 2.2,
-      this.monoImage.height / 2.2
+      this.monoImage.width,
+      this.monoImage.height
     );
   }
 
